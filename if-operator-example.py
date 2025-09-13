f1dd4c274710d0ca7ef6d0ef0d0db1bc8595ab53
Aymogul <aymogul22@gmail.com>
2025-09-13 16:57:43 +0100

    saved

--- if-operator-example.py
+++ if-operator-example.py
@@ -10,3 +10,20 @@ number2 = int(input('Enter the second integer: '))
 
 if number1 == number2:
     print(number1, 'is equal to', number2)
+
+if number1 != number2:
+    print(number1, 'is not equal to', number2)
+
+if number1 < number2:
+    print(number1, 'is less than', number2)
+
+if number1 > number2:
+    print(number1, 'is greater than', number2)
+
+if number1 <= number2:
+    print(number1, 'is less than or equal to', number2)
+
+if number1 >= number2:
+    print(number1, 'is greater than or equal to', number2)
+
+
