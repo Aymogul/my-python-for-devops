ae0fbb2fd7922fa6c8de735fb344e87481aacfc3
Aymogul <aymogul22@gmail.com>
2025-09-15 00:41:34 +0100

    assign and equals to

--- operators.py
+++ operators.py
@@ -18,3 +18,8 @@ x == y
 
 # not equal to 
 x != y
+
+# the assign and equals to difference
+x == y # this means the value of x and y are the same 
+
+x = y # the value assigned to x is y in this case
