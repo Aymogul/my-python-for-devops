41e21e9ec2a5bbbfcb3d4d524dc34567e90fbd78
Aymogul <aymogul22@gmail.com>
2025-09-16 00:26:01 +0100

    new

--- control-statement.py
+++ control-statement.py
@@ -1,1 +1,19 @@
 # This chapter 3 will cover the control statement with lots of python keyword to be considered to be considered: lets get started
+grade = 85
+
+if grade >= 60:
+    print('Passed')
+
+# Suite Indentation
+if grade >= 60:
+    print('Passed')  # statement is not indented properly
+
+if grade >= 60:
+    print('Passed')
+
+# if Statement Flowchart
+if 1:
+    print('Nonzero values are true, so this will print')
+    
+if 0:
+    print('Zero is false, so this will not print')
