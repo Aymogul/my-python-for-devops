3752874ee541f660351cc4cf887a4274676d9c48
Aymogul <aymogul22@gmail.com>
2025-09-16 00:30:09 +0100

    if and else statement

--- if-else.py
+++ if-else.py
@@ -18,3 +18,31 @@ if grade >= 60:
     print('Passed')
 else:
     print('Failed')
+
+# Conditional Expressions
+
+grade = 87
+
+if grade >= 60:
+    result = 'Passed'
+else:
+    result = 'Failed'
+    
+result
+
+result = ('Passed' if grade >= 60 else 'Failed')
+
+result
+
+'Passed' if grade >= 60 else 'Failed'
+
+# Multiple Statements in a Suite
+
+grade = 49
+
+if grade >= 60:
+    print('Passed')
+else:
+    print('Failed')
+    print('You must take this course again')
+    
