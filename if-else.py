6d791c010ddf0def45f591c387a1bab938d41339
Aymogul <aymogul22@gmail.com>
2025-09-16 00:29:09 +0100

    news-hja

---
+++ if-else.py
@@ -0,0 +1,20 @@
+grade = 85
+
+if grade >= 60: 
+    print('Passed')
+else:
+    print('Failed')
+
+grade = 57
+
+if grade >= 60: 
+    print('Passed')
+else:
+    print('Failed')
+
+grade = 99
+
+if grade >= 60: 
+    print('Passed')
+else:
+    print('Failed')
