9106eb20ec96b35736ae8eb2028659b2aeea2325
Aymogul <aymogul22@gmail.com>
2025-09-17 01:06:11 +0100

    new line-k

--- for-in.py
+++ for-in.py
@@ -1,2 +1,10 @@
+# the basic idea behind for loop is iteration
+# for variable in sequence_name
+
+
 for character in 'Programming':
-    print(character, end=' ')
+    print(character, end='  ')
+
+names = ['ayo', 'deji', 'ay', 'ade']
+for name in names:
+    print(name)
