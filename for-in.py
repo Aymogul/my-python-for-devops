cc4e12f3eb717dea0afb23ea4cbae0ec90186816
Aymogul <aymogul22@gmail.com>
2025-09-17 01:15:30 +0100

    new line-of loop

--- for-in.py
+++ for-in.py
@@ -5,6 +5,25 @@
 for character in 'Programming':
     print(character, end='  ')
 
+
 names = ['ayo', 'deji', 'ay', 'ade']
 for name in names:
     print(name)
+
+for char in "hello":
+    print(char)
+
+
+for i in range(7):
+    print(i)
+
+
+for num in range(10):
+    if num == 5:
+        break
+    print(num)
+
+for num in range(5):
+    if num == 2:
+        continue
+    print(num)
