c082bc790a627ff3da45f9fb1c35badbcd8c5afb
Aymogul <aymogul22@gmail.com>
2025-09-20 02:12:29 +0100

    omoo

--- import-decimal.py
+++ import-decimal.py
@@ -17,3 +17,5 @@ print(x)
 for year in range(1, 11):
     amount = principal * (1 + rate) ** year 
     print(f'{year:>2}{amount:>10.2f}')
+    # {amoun:10.2f} means:in a field of width 10 characters
+    # .2f = format as a fixed-point number with 2 decimal places
