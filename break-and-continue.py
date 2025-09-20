31b6708d7f4536f74cc82c054c04143117a60f7e
Aymogul <aymogul22@gmail.com>
2025-09-20 02:20:43 +0100

    break-continue

---
+++ break-and-continue.py
@@ -0,0 +1,8 @@
+for number in range(100):
+    if number == 10:
+        break
+    print(number, end=' ')
+for number in range(10):
+    if number == 5:
+        continue
+    print(number, end=' ')
