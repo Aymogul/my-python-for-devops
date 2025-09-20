06eefa41236764e38bae4c698e126262e31f5439
Aymogul <aymogul22@gmail.com>
2025-09-20 01:44:46 +0100

    leggo

--- import-decimal.py
+++ import-decimal.py
@@ -3,3 +3,12 @@ from decimal import Decimal
 principal = Decimal('1000.00')
 
 print(principal)
+
+rate = Decimal('0.05')
+print(rate)
+
+x = Decimal('10.5')
+y = Decimal('2')
+print(x + y)
+print(x // y)
+print(x)
