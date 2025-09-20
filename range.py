d8c88ba8b6e0ba52002373e1c41e916a19233ca4
Aymogul <aymogul22@gmail.com>
2025-09-20 01:19:59 +0100

    rangee-2

--- range.py
+++ range.py
@@ -3,12 +3,18 @@ for number in range(0, 10):
 
 for number in range(5, 10):
     print(number, end=' ')
-    # start = 5
-    # stop before 10 
+    # start = 5 → begin from 5
+    # stop = 10 → go up to but not including 10
+    # step = 1 (default)
 
 
 for number in range(0, 10, 2):
     print(number, end=' ')
+    # start = 0
+    # stop = stop before 10
+    # step by 2
+
+
 
 
 for number in range(10, 0, -2):
