562a18181f4e704170b8f4ea7403d3e11ce3f275
Aymogul <aymogul22@gmail.com>
2025-09-20 01:17:08 +0100

    rangee

--- range.py
+++ range.py
@@ -1,2 +1,15 @@
 for number in range(0, 10):
     print(number)
+
+for number in range(5, 10):
+    print(number, end=' ')
+    # start = 5
+    # stop before 10 
+
+
+for number in range(0, 10, 2):
+    print(number, end=' ')
+
+
+for number in range(10, 0, -2):
+    print(number, end=' ')
