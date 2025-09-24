90d9e4681d587a007a30b5704710ee2e0ad56113
Aymogul <aymogul22@gmail.com>
2025-09-25 00:09:52 +0100

    function

---
+++ function.py
@@ -0,0 +1,11 @@
+def square(number):
+    """Calculate the square of number."""
+    return number ** 2
+
+square(7)
+
+square(2.5)
+
+# Returning a Result to a Function’s Caller
+print('The square of 7 is', square(7))
+
