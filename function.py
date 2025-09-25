8b39364a186252e92ffdc4c84595c203c6d9a7ad
Aymogul <aymogul22@gmail.com>
2025-09-26 00:47:17 +0100

    function of square

--- function.py
+++ function.py
@@ -16,3 +16,12 @@ print('The square of 2.5 is', square(2.5))
 # the function parameter or variable is number
 # the return is the answer to the execution of the function 
 
+def square(number: float) -> float:
+    """Calculate the square of a number."""
+    return number ** 2
+square(5)
+square(4)
+
+print('The square of 5 is', square(5))
+
+print('The square of 4 is', square(4))
