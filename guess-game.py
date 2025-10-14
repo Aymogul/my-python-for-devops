98da75f968a89ffb425ccc9248a83b230dd48d69
Aymogul <aymogul22@gmail.com>
2025-10-14 01:40:04 +0100

    new

---
+++ guess-game.py
@@ -0,0 +1,18 @@
+import random
+
+lower_bound = 1
+upper_bound = 1000
+max_attempts = 10
+
+secret_number = random.randint(lower_bound, upper_bound)
+
+def get_guess():
+    while True:
+        try:
+            guess = int(input(f"Guess a number between {lower_bound} and {upper_bound}: "))
+            if lower_bound <= guess <= upper_bound:
+                return guess
+            else:
+                print("Invalid input. Please enter a number within the specified range.")
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
