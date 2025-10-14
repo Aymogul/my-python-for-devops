99b3901575f57fb640a9cf977d04fb8949ef2772
Aymogul <aymogul22@gmail.com>
2025-10-14 02:10:24 +0100

    die--ce

--- die-dice.py
+++ die-dice.py
@@ -9,3 +9,22 @@ frequency3 = 0
 frequency4 = 0
 frequency5 = 0
 frequency6 = 0
+
+
+# 6,000,000 die rolls
+for roll in range(6_000_000):  # note underscore separators
+    face = random.randrange(1, 7)
+
+    # increment appropriate face counter
+    if face == 1:
+        frequency1 += 1
+    elif face == 2:
+        frequency2 += 1
+    elif face == 3:
+        frequency3 += 1
+    elif face == 4:
+        frequency4 += 1
+    elif face == 5:
+        frequency5 += 1
+    elif face == 6:
+        frequency6 += 1
