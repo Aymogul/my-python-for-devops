3bafce6b62c411b7e3a72ff5ce895e4fe4a81704
Aymogul <aymogul22@gmail.com>
2025-10-14 02:09:36 +0100

    die

---
+++ die-dice.py
@@ -0,0 +1,11 @@
+# fig04_01.py
+"""Roll a six-sided die 6,000,000 times."""
+import random
+
+# face frequency counters
+frequency1 = 0
+frequency2 = 0
+frequency3 = 0
+frequency4 = 0
+frequency5 = 0
+frequency6 = 0
