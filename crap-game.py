40dd928b3e07c2b507e7264422a7e1741319dada
Aymogul <aymogul22@gmail.com>
2025-10-16 02:10:20 +0100

    no crap

--- crap-game.py
+++ crap-game.py
@@ -1,2 +1,8 @@
 """Simulating the dice game Craps."""
 import random
+
+def roll_dice():
+    """Roll two dice and return their face values as a tuple."""
+    die1 = random.randrange(1, 7)
+    die2 = random.randrange(1, 7)
+    return (die1, die2)  # pack die face values into a tuple
